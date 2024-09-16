4738b4abb7af9caa543a98e8c3ce57429022ff6a
Buzdi <103930136+Buzdi@users.noreply.github.com>
2024-09-16 14:34:15 +0300

    AC Code

--- Closest Cow Wins/main.cpp
+++ Closest Cow Wins/main.cpp
@@ -3,40 +3,27 @@
 #include <cassert>
 #include <vector>
 #define ll long long
-#define int long long
 
 using namespace std;
 
-const int NMAX = 3e5;
+const int NMAX = 2e5;
+const int INF = 2e9;
 
-struct Patch {
-    int p, t;
-    void read() {
-        cin >> p >> t;
-    }
-    bool operator < (const Patch &other) const {
-        return p < other.p;
-    }
-}patches[NMAX + 1];
+int k, m, n, ind, L, R;
+int cows[NMAX + 2];
+vector<pair<ll, int>> values;
+ll sum_solo, sum_double, answer;
+pair<int, int> patches[NMAX + 1];
 
-struct Event {
-    int pos;
-    ll value;
-    bool operator < (const Event &other) const {
-        if(pos < other.pos) {
-            return true;
-        }
-        if(pos == other.pos && value < 0) {
-            return true;
-        }
-        return false;
+bool cmp(pair<ll, int> a, pair<ll, int> b) {
+    if(a.first > b.first) {
+        return true;
     }
-};
-
-int k, m, n, ind;
-ll answer, sum_values;
-int cows[NMAX + 1];
-vector<Event> events;
+    if(a.first == b.first && a.second < b.second) {
+        return true;
+    }
+    return false;
+}
 
 signed main()
 {
@@ -46,40 +33,69 @@ signed main()
 
     cin >> k >> m >> n;
     for(int i = 1; i <= k; i++) {
-        patches[i].read();
+        cin >> patches[i].first >> patches[i].second;
     }
     for(int i = 1; i <= m; i++) {
         cin >> cows[i];
     }
     sort(patches + 1, patches + k + 1);
+    sort(cows + 1, cows + m + 1);
 
-    for(int i = 1; i <= k; i++) {
-        int pos = lower_bound(cows + 1, cows + m + 1, patches[i].p) - cows;
-        if(pos == m + 1) {
-            assert(cows[m] < patches[i].p);
-            int distance = patches[i].p - cows[m];
-            events.push_back({patches[i].p - distance, patches[i].t});
-            events.push_back({patches[i].p + distance, -patches[i].t});
+    /// Left interval
+    R = 1;
+    while(patches[R].first < cows[1]) {
+        sum_solo += patches[R].second;
+        R++;
+    }
+    values.push_back({sum_solo, 1});
+
+    /// Right interval
+    L = k;
+    sum_solo = 0;
+    while(patches[L].first > cows[m]) {
+        sum_solo += patches[L].second;
+        L--;
+    }
+    values.push_back({sum_solo, 1});
+
+    /// Middle intervals
+    for(int i = 1; i <= m - 1; i++) {
+        if(cows[i] + 1 == cows[i + 1]) {
+            continue;
         }
-        else if(pos == 1) {
-            assert(cows[1] > patches[i].p);
-            int distance = cows[1] - patches[i].p;
-            events.push_back({patches[i].p - distance, patches[i].t});
-            events.push_back({patches[i].p + distance, -patches[i].t});
+
+        L = lower_bound(patches + 1, patches + k + 1, make_pair(cows[i], 0)) - patches;
+        R = L;
+        sum_double = 0;
+        while(patches[R].first < cows[i + 1]) {
+            sum_double += patches[R].second;
+            R++;
         }
-        else {
-            assert(cows[pos - 1] < patches[i].p && patches[i].p < cows[pos]);
-            int min_distance = min(cows[pos] - patches[i].p, patches[i].p - cows[pos - 1]);
-            events.push_back({patches[i].p - min_distance, patches[i].t});
-            events.push_back({patches[i].p + min_distance, -patches[i].t});
+
+        sum_solo = 0;
+        R = L;
+        int pos = cows[i] + 1;
+        ll max_sum = 0;
+        while(patches[R].first < cows[i + 1]) {
+            while(2 * patches[R].first < cows[i + 1] + pos) {
+                sum_solo += patches[R].second;
+                R++;
+            }
+            while(2 * patches[L].first <= cows[i] + pos) {
+                sum_solo -= patches[L].second;
+                L++;
+            }
+            max_sum = max(max_sum, sum_solo);
+            pos++;
         }
+
+        values.push_back({sum_double - max_sum, 2});
+        values.push_back({max_sum, 1});
     }
 
-    sort(events.begin(), events.end());
-    for(auto event : events) {
-        cout << event.pos << ' ' << event.value << '\n';
-        sum_values += event.value;
-        answer = max(answer, sum_values);
+    sort(values.begin(), values.end(), cmp);
+    for(int i = 0; i < min(n, (int) values.size()); i++) {
+        answer += values[i].first;
     }
     cout << answer << '\n';
 
